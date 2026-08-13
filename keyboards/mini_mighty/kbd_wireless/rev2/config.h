f6d4488adfde974255506918896a721948bcf38c
dmlz76 <dimitar.lazarov@usa.net>
2026-08-13 11:39:17 -0700

    Increased WDT timer to 1s for kbd_wireless rev2 since it supports interrupt wakeup.

--- keyboards/mini_mighty/kbd_wireless/rev2/config.h
+++ keyboards/mini_mighty/kbd_wireless/rev2/config.h
@@ -4,3 +4,8 @@
 
 #define RST_PIN C6
 #define SLEEP_PIN D7
+
+// Watchdog period used for MCU power-down sleeps. Async wake
+// handles button/encoder events regardless of this value, so it only bounds
+// the timer drift correction window in WDT_vect. See ble_send_buf.cpp.
+#define MCU_POWER_DOWN_WDTO WDTO_1S
